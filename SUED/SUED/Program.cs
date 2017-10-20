d5e238cac0b813e8970f663b17741da736224097
Bruno Freitas <brunosavyofs@gmail.com>
2017-10-19 23:31:24 -0300

    Inclui comentários no desafio SUED

--- SUED/SUED/Program.cs
+++ SUED/SUED/Program.cs
@@ -12,10 +12,9 @@ namespace SUED
         #region Declaração de constantes e variáveis
         private const int SEGUNDOS_PENSANDO = 3;
         private const string saudacao = "Espelho, espelho meu, me responda ";
-        private const string pergunta = "Faça qualquer pergunta (Use a tecla End para encerrar):";
+        private const string pergunta = "Faça qualquer pergunta:";
 
         private static string resposta;
-        private static Boolean encerrar = false;
         private static Boolean capturarResposta = false;
         #endregion
 
@@ -23,25 +22,23 @@ namespace SUED
         {
             do
             {
-                Console.WriteLine(pergunta);
-                if (CapturarResposta())
-                {
-                    Perguntar();
-                    Pensar();
-                    Responder();
-                } else
-                {
-                    break;
-                }
+                Perguntar();
+                Pensar();
+                Responder();
             } while (true);
         }
 
         private static void Perguntar()
         {
+            Console.WriteLine(pergunta);
+            CapturarResposta();
             Console.ReadLine();
             Console.WriteLine();
         }
 
+        /// <summary>
+        /// Escreve a resposta na tela e reseta a variável contendo a resposta digitada
+        /// </summary>
         private static void Responder()
         {
             Console.WriteLine(" => {0} <=", resposta);
@@ -50,43 +47,47 @@ namespace SUED
         }
 
         /// <summary>
-        /// Captura a resposta da pergunta.
+        /// Captura a resposta da pergunta enquanto exibe a saudação na tela.
+        /// Ao ser pressionada a tecla ';', a captura será iniciada.
+        /// Ao ser pressionada a tecla ENTER, a captura será encerrada.
+        /// Ao término da frase de saudação/elogio, o cursor exibirá exatamente o que for digitado.
         /// </summary>
-        /// <returns>False caso a resposta não seja </returns>
-        private static Boolean CapturarResposta()
+        private static void CapturarResposta()
         {
             for (var i = 0; i < saudacao.Length; i++)
             {
+                // Captura a tecla pressionada
                 ConsoleKeyInfo key = Console.ReadKey(true);
                 char ch = key.KeyChar;
 
                 if (capturarResposta)
                 {
+                    // Caso a captura da resposta já tenha sido iniciada
+
                     if (key.Key == ConsoleKey.Enter)
                     {
+                        // Caso o ENTER tenha sido pressionada, encerra captura da resposta
                         capturarResposta = false;
                     }
                     else
                     {
+                        // Se não for a tecla ENTER, continua capturando a resposta
                         resposta += ch;
                     }
                 }
                 else if (ch == ';')
                 {
+                    // Se ainda não tiver sido iniciada a captura da resposta e for pressionada tecla ';', inicia captura da resposta
                     capturarResposta = true;
                 }
 
-                // Encerra a execução se a tecla End for pressionada
-                if (key.Key == ConsoleKey.End)
-                {
-                    return false;
-                }
                 Console.Write(saudacao[i]);
             }
-
-            return true;
         }
 
+        /// <summary>
+        /// Exibe ao usuário a mensagem para aguardar a resposta.
+        /// </summary>
         private static void Pensar()
         {
             Console.Write("Pensando");
@@ -97,7 +98,6 @@ namespace SUED
                 Thread.Sleep(1000);
                 x++;
             } while (x < SEGUNDOS_PENSANDO);
-            //Console.WriteLine();
         }
     }
 }
