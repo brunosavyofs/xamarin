5ea0826db0f75a5a75894ca5383d3289cae378c1
BRUNO SILVA <FP20160210@ACADEMICO.LOCAL>
2017-10-22 09:42:41 -0200

    Inclui projeto de exemplo de webservice

---
+++ WebService/WebService/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebService
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string url = "https://www.osvigaristas.com.br/piadas";
+            WebClient webClient = new WebClient();
+        }
+    }
+}
